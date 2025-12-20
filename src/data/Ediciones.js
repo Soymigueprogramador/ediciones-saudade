7b348862b24d08c5597dccc5e331cf035ed960fc
Miguel Salazar <yobebe@hotmail.com.ar>
2025-12-20 12:48:23 -0300

    Probando

--- src/data/Ediciones.js
+++ src/data/Ediciones.js
@@ -1,20 +1,244 @@
-// Conjunto de objetos que mostrará los productos
+// Array de objetos con los productos que simula la base de datos
 
 export const Ediciones = [
   {
     id: 1,
     name: "edicion 1",
-    description: "Doble shot de espresso con leche microespumada, suave y balanceado.",
-    price: 3.50,
-    image: "/public/assets/ediciones/edicion-1.jpeg",
-    category: "Bebidas Calientes"
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/src/assets/ediciones/edicion-1.jpeg",
+    category: "Ediciones"
   },
   {
     id: 2,
     name: "edicion 2",
-    description: "Masa hojaldrada, suave y mantecosa, recién horneado.",
-    price: 2.75,
-    image: "/public/assets/ediciones/edicion-2.jpeg",
-    category: "Pastelería"
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/src/assets/ediciones/edicion-2.jpeg",
+    category: "Ediciones"
   },
+  {
+    id: 3,
+    name: "edicion 3",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-3.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 4,
+    name: "edicion 4",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-4.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 5,
+    name: "edicion 5",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-5.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 6,
+    name: "edicion 6",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-6.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 7,
+    name: "edicion 7",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-7.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 8,
+    name: "edicion 8",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-8.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 9,
+    name: "edicion 9",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-9.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 10,
+    name: "edicion 10",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-10.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 11,
+    name: "edicion 11",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-11.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 12,
+    name: "edicion 12",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-12.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 13,
+    name: "edicion 13",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-13.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 14,
+    name: "edicion 14",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-14.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 15,
+    name: "edicion 15",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-15.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 16,
+    name: "edicion 16",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-16.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 17,
+    name: "edicion 17",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-17.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 18,
+    name: "edicion 18",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-18.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 19,
+    name: "edicion 19",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-19.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 20,
+    name: "edicion 20",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-20.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 21,
+    name: "edicion 21",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-21.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 22,
+    name: "edicion 22",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-22.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 23,
+    name: "edicion 23",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-23.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 24,
+    name: "edicion 24",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-24.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 25,
+    name: "edicion 25",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-25.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 26,
+    name: "edicion 26",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-26.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 27,
+    name: "edicion 27",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-27.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 28,
+    name: "edicion 28",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-28.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 29,
+    name: "edicion 29",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-29.jpeg",
+    category: "Ediciones"
+  },
+  {
+    id: 30,
+    name: "edicion 30",
+    description: "Edicion perfecta para buzos y remeras",
+    price: 700,
+    image: "/public/assets/ediciones/edicion-30.jpeg",
+    category: "Ediciones"
+  }
 ];
